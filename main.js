553306f94eccadb72734fedc9c804baf0720c6fb
Dorian Dubreus <doriandubreus@gmail.com>
2022-04-25 14:44:54 -0400

    student final grade solution 8kyu

--- main.js
+++ main.js
@@ -444,3 +444,17 @@ function friend(friends) {
     return friendsArr
 }
 
+//STUDENTS FINAL GRADE 
+function finalGrade(exam, projects) {
+    //store the final grade 
+    if (exam > 90 || projects > 10) {
+        return 100
+    } else if (exam > 75 && projects >= 5) {
+        return 90
+    } else if (exam > 50 && projects >= 2) {
+        return 75
+    } else {
+        return 0
+    }
+
+}
