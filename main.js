1dbc6b235d253bcf3f35ec8f12933701be878711
Dorian Dubreus <doriandubreus@gmail.com>
2022-04-28 16:32:48 -0400

    list filtering solution 7kyu

--- main.js
+++ main.js
@@ -554,3 +554,9 @@ function calculateYears(principal, interest, tax, desired) {
 
 }
 
+
+//List Filtering 
+function filter_list(l) {
+    // Return a new array with the strings filtered out
+    return l.filter(str => str !== str.toString())
+}
