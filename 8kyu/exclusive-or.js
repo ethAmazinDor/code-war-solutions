d00df76409f23846d93c45286b52b8c88018f7a5
Dorian Dubreus <doriandubreus@gmail.com>
2022-05-12 23:43:42 -0400

    exclusive or 8kyu solution

---
+++ 8kyu/exclusive-or.js
@@ -0,0 +1,13 @@
+function xor(a, b) {
+    // TODO: Program Me
+    if ((a === true && b === false) ||
+        (a === false && b === true)) {
+        return true
+    }
+    return false
+}
+
+
+function xor(a, b) {
+    return a != b
+}
