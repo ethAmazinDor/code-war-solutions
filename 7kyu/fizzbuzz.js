4c2a0ce95448ea54b27bd1b718b239a9bf004ced
Dorian Dubreus <doriandubreus@gmail.com>
2022-05-20 22:58:24 -0400

    fizzbuzz 7kyu solution

---
+++ 7kyu/fizzbuzz.js
@@ -0,0 +1,19 @@
+// Return an array
+function fizzbuzz(n) {
+
+    let newArr = []
+
+    for (let i = 1; i <= n; i++) {
+        num = i
+        if (num % 3 === 0 && num % 5 === 0) {
+            num = 'FizzBuzz'
+        } else if (num % 3 === 0) {
+            num = 'Fizz'
+        } else if (num % 5 === 0) {
+            num = 'Buzz'
+        }
+        newArr.push(num)
+    }
+
+    return newArr
+}
