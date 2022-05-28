d602ac480e710003cf2a179efb93a5f7bf2da3cb
Dorian Dubreus <doriandubreus@gmail.com>
2022-05-27 22:31:06 -0400

    reverse the number 7kyu solution

---
+++ 7kyu/reverse-number.js
@@ -0,0 +1,9 @@
+function reverseNumber(n) {
+    let isNegative = n < 0
+
+    let reverseAsString = Math.abs(n).toString().split('').reverse.join('')
+    let result = Number(reverseAsString)
+
+    return isNegative ? -result : result
+
+}
