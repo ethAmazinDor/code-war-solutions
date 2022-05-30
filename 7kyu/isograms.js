a738b01d4249efa08bb3f3bf65c72257a018c332
Dorian Dubreus <doriandubreus@gmail.com>
2022-05-29 22:36:16 -0400

    first attempt at isograms

---
+++ 7kyu/isograms.js
@@ -0,0 +1,22 @@
+function isIsogram(str) {
+    //convert the string to lowercase or uppercase 
+    //add the elements into an array 
+
+
+    let i
+    let j
+    str.toLowerCase()
+    //sort the elements in the array  
+    for (i < str.length; ++i;) {
+        for (j = j + 1; j < str.length; ++j)
+            if (str[i] == str[j])
+                return false
+
+        return true
+
+
+    }
+    //check if the first index is equal to the zero index
+
+
+}
