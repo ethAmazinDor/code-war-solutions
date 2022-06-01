d05e95afe58fb29ba5fef79cdd6b7cc9d8c63800
Dorian Dubreus <doriandubreus@gmail.com>
2022-06-01 12:22:04 -0400

    string drills 7kyu solution

---
+++ 7kyu/string-drills.js
@@ -0,0 +1,21 @@
+/*
+
+Write a function named repeater() that takes two arguments (a string and a number),
+ and returns a new string where the input string is repeated that many times.
+
+
+*/
+
+function repeater(string, n) {
+    //create a variable to hold the string 
+    //  let emptyStr = ''
+    //     //run a for loop 
+    //     for(let i = 0; i < n; i++){
+    //           emptyStr += string
+    //     }
+    //  return emptyStr
+
+
+    return string.repeat(n)
+
+}
